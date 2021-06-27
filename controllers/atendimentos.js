c2f77b95f9c07da6246c420a0de66f21e1e58fad
Manuella Guimarães <manuellasgg@outlook.com>
2021-06-27 07:16:10 -0300

    refatorando o código (data, status, validacoes)

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,10 +4,9 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
     
     app.post('/atendimentos', (req, res) => {
-        const atendimento = req.body //conteúdo do body é um atendimento
+        const atendimento = req.body //atendimento recebe o conteúdo do body
 
-        Atendimento.adiciona(atendimento)
-        res.send("Post atendimento") //para criar um novo atendimento e consumir essa rota usando o Postman
+        Atendimento.adiciona(atendimento, res)
     })
 
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,16 +1,48 @@
+const moment = require('moment')
+
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento) {
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss') //como está vazio, ele devolverá a data atual
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss') //data recebe a data que está no atendimento e a moment formata ela
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao) //validação da data, retorna true se a data for igual ou posterior à dataCriacao
+        const clienteEhValido = atendimento.cliente.length >= 5
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
-            if(erro) {
-                console.log(erro)
-            } else {
-                console.log(resultados)
+        const validacoes = [
+            {
+                nome: 'data', //nome do campo que estamos validando é data
+                valido: dataEhValida,
+                mensagem: 'Data deve ser igual ou posterior à data atual' //mensagem de erro
+
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido) //se não for válido, o campo será retornado e ficará salvo dentro dos erros
+        const existemErros = erros.length
+
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data} //array com tudo que estiver dentro de atendimento e uma data de criação
+       
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro) //alterando status HTTP
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }
+
     }
 }
 
